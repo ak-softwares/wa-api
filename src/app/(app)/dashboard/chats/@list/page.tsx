d23d640be8755847f79a441e0f00d6edb9dfa13f
aramarket <aramarket.in@gmail.com>
2025-11-06 21:01:49 +0530

    update2

--- src/app/(app)/dashboard/chats/@list/page.tsx
+++ src/app/(app)/dashboard/chats/@list/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-import { Suspense } from "react";
 import ChatList from "@/components/dashboard/chats/chatList";
 import { ChatsProvider } from "@/hooks/chat/ChatsContext";
-import { useSearchParams } from "next/navigation";
 
-export default function ChatPage() {
-  const searchParams = useSearchParams();
-  const phone = searchParams.get("phone") || undefined;
+export default function ChatPage({
+  searchParams,
+}: {
+  searchParams: { phone?: string };
+}) {
+  const phone = searchParams.phone || undefined;
 
   return (
-    <Suspense>
       <ChatsProvider phone={phone}>
         <ChatList />
       </ChatsProvider>
-    </Suspense>
   );
 }
